b9b703b2a99ddd83075c4b7e9333c210e60a04a6
Simon <zibo99@gmail.com>
2018-07-14 13:40:46 +0500

    Initial commit

---
+++ InterfaceTest1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfaceTest1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        interface IList
+        {
+            string Text { get; set; }
+        }
+
+        class MyList : IList
+        {
+            public string Text { get; set; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IList l = new MyList();
+            l.Text = "Запись 1";
+            textBox1.Text += l.Text + "\r\n";
+
+            MyList ml = new MyList();
+            ml.Text = "Запись 2";
+            textBox1.Text += ml.Text;
+        }
+    }
+}
